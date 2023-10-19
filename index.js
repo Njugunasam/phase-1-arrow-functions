a070182184628a58c46af319e3d50dbfe951a173
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-19 20:20:02 +0300

    correcting errors

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a, b) {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+  }
+  
+  const square = (x) => x * x;
+  
+  const add = (a, b) => a + b;
+  
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
